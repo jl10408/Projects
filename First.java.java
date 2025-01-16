5d446a2ed07fc02933af128ee2c123efee4a383e
jl10408 <ligon_joshua@students.columbusstate.edu>
2025-01-16 18:00:48 -0500

    Create First.java.java
    This program is for assignment 1.

---
+++ First.java.java
@@ -0,0 +1,10 @@
+// Joshua Ligon
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("Job Title: Information Security Analyst");
+        System.out.println("This job has a growth projection of 33% (Great outlook)");
+        System.out.println("The median salary is $120360 or $57.87 per hour");
+    }
+}
