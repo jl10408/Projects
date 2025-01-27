043e8d42fb1a6eae6f4b83b9948887f9267e54e8
jl10408 <ligon_joshua@students.columbusstate.edu>
2025-01-27 01:08:06 -0500

    new

---
+++ Tests.java
@@ -0,0 +1,17 @@
+public class Tests {
+    public static void main(String[] args) {
+        // Declare test scores
+        double score1 = 66.2;
+        double score2 = 60.9;
+        double score3 = 37.6;
+
+        // Calculate the average
+        double average = (score1 + score2 + score3) / 3;
+
+        // Display test scores and average
+        System.out.println("Test score 1:   " + score1);
+        System.out.println("Test score 2:   " + score2);
+        System.out.println("Test soxre 3:   " + score3);
+        System.out.println("The average of 3 test scores is:   " + average);
+    }
+}
