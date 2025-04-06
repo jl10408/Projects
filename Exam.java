94188f1678b25f49f153eef160e7fcf404e0b939
jl10408 <ligon_joshua@students.columbusstate.edu>
2025-04-06 19:40:01 -0400

    Update Exam.java
    This is an updated version to meet the requirements in module 7 assignment.

--- Exam.java
+++ Exam.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 public class Exam extends Assessment implements Graded {
     private int numQuestions;
     private int numMissed;
@@ -7,20 +9,38 @@ public class Exam extends Assessment implements Graded {
         numQuestions = totalQuestions;
         numMissed = questionsMissed;
 
-        // Calculating and setting the score **
-        int theScore = 100 - (numMissed * getPointsEach());
+        int theScore;
+        try {
+            theScore = 100 - (numMissed * getPointsEach());
+        } catch (ArithmeticException e) {
+            theScore = 0;
+            JOptionPane.showMessageDialog(null, "Error: Number of questions cannot be zero.");
+        }
         setScore(theScore);
     }
 
     // Method to find how many points each question is worth ##
     public int getPointsEach() {
-        return 100 / numQuestions;
+        try {
+            return 100 / numQuestions;
+        } catch (ArithmeticException e) {
+            JOptionPane.showMessageDialog(null, "Error: Division by zero encountered. Number of questions must not be zero.");
+            return 0;
+        }
     }
 
-    //edit
     @Override
     public char getLetterGrade() {
         return getGrade();
+    }
+
+    public static int safeParseInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Error: Please ennter a valid integer.");
+            return -1;
+        }
     }
 }
 
